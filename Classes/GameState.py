eed935f5fc2545d34e490ce643511a3f22d53557
Linn <linn.martensson@hotmail.se>
2024-08-30 11:35:09 +0200

    added class GameState and lists for holding players, game deck and build piles

---
+++ Classes/GameState.py
@@ -0,0 +1,5 @@
+class GameState:
+    """Game state will hold a list of players and a list with all the available cards in the common game deck and the cards already placed in the build piles"""
+    players = []
+    game_deck = []
+    build_piles = [[]]
