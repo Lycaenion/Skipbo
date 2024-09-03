b84ddc6fcd4788aac39476aa3d8af5ac98aa9134
Linn <linn.martensson@hotmail.se>
2024-09-03 09:44:15 +0200

    refactoring

--- Classes/Game.py
+++ Classes/Game.py
@@ -1,9 +1,8 @@
 """Game will hold actions, handle logic and communicate with the game state."""
-from Classes.Player import Player
-
 """Game will create players with decks, game deck and pass it along to the game state."""
 """Actions available are: Build, discard and draw. Build is available from both player hand and player discard piles. When player runs out of cards on hand, 5 new ones will be given and the player continues."""
 """The game will first be adapted for 2-3 players. Later players 4-6 will be added."""
+from Classes.Player import Player
 import Card
 import random
 CARD_VALUES = {
