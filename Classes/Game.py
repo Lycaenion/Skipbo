068cff060864f5cb884c5632de6bbeee38ae89ff
Linn <linn.martensson@hotmail.se>
2024-09-03 09:18:24 +0200

    create players and populate decks and hands, removing cards from common deck

--- Classes/Game.py
+++ Classes/Game.py
@@ -1,4 +1,6 @@
 """Game will hold actions, handle logic and communicate with the game state."""
+from Classes.Player import Player
+
 """Game will create players with decks, game deck and pass it along to the game state."""
 """Actions available are: Build, discard and draw. Build is available from both player hand and player discard piles. When player runs out of cards on hand, 5 new ones will be given and the player continues."""
 """The game will first be adapted for 2-3 players. Later players 4-6 will be added."""
@@ -19,12 +21,21 @@ CARD_VALUES = {
     'TWELVE' : 12
 }
 
+PLAYER_DECK_SIZE = 30
+PLAYER_HAND_SIZE = 5
+
 game_deck = []
 players = []
 
 number_of_players = 2
 
 def create_game():
+    for i in range(number_of_players):
+        player_deck = populate_player_deck()
+        player_hand = populate_player_hand()
+        player = Player(i,player_hand, player_deck)
+        players.append(player)
+
     return
 
 def populate_game_deck():
@@ -42,10 +53,26 @@ def populate_game_deck():
     return
 
 def populate_player_deck():
-    return
+
+    player_deck = []
+    for i in range(PLAYER_DECK_SIZE):
+        player_deck.append(game_deck[i])
+
+    for i in range(PLAYER_DECK_SIZE):
+        game_deck.pop(i)
+
+    return player_deck
 
 def populate_player_hand():
-    return
+    player_hand = []
+
+    for i in range(PLAYER_HAND_SIZE):
+        player_hand.append(game_deck[i])
+
+    for i in range(PLAYER_HAND_SIZE):
+        game_deck.pop(i)
+
+    return player_hand
 
 def build():
     return
